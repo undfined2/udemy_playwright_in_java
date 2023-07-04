46acc6b0698f07fb7babe9b22e0632bc54434ae3
Stophe Bamberry <chris.bamberry@digio.com.au>
2023-07-04 12:33:00 +1000

    pom.xml | add junit & playwright to project
    FirstTest.java: basic test to open chromium browser with head (debug on println() to view).

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,19 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+
+        page.navigate("https://google.com");
+
+        System.out.println();
+    }
+}
